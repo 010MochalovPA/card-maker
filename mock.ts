5e380e7782ad83a13f110d821d955187bdb2f53c
vladislav@book <vladovinkin@gmail.com>
2023-10-04 06:41:31 +0300

    обработал проект Prettier'ом

--- mock.ts
+++ mock.ts
@@ -1,351 +1,365 @@
 import {
-    SlideObjectType,
-    SlideBackgroundType,
-    ShapeType,
-    PictureType,
-    ShapeObject,
-    PictureObject,
-    TextObject,
-    Slide,
-    Doc,
-    Editor
-} from "./types"
+  SlideObjectType,
+  SlideBackgroundType,
+  ShapeType,
+  PictureType,
+  ShapeObject,
+  PictureObject,
+  TextObject,
+  Slide,
+  Doc,
+  Editor,
+} from './types'
 
-const picture1 : PictureObject = {
-    id: "a38883aff79d4daf8ce292a0a8353b7f",
-    type: SlideObjectType.PICTURE,
-    pictureType: PictureType.BASE64,
-    size: {
-        width: 100,
-        height: 100,
-    },
-    position: {
-        x: 30,
-        y: 45,
-    },
-    angle: 10,
-    data: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABmJLR0QA/wD/AP+gvaeTAAAAB3RJTUUH1ggDCwMADQ4NnwAAAFVJREFUGJWNkMEJADEIBEcbSDkXUnfSgnBVeZ8LSAjiwjyEQXSFEIcHGP9oAi+H0Bymgx9MhxbFdZE2a0s9kTZdw01ZhhYkABSwgmf1Z6r1SNyfFf4BZ+ZUExcNUQUAAAAASUVORK5CYII=",
+const picture1: PictureObject = {
+  id: 'a38883aff79d4daf8ce292a0a8353b7f',
+  type: SlideObjectType.PICTURE,
+  pictureType: PictureType.BASE64,
+  size: {
+    width: 100,
+    height: 100,
+  },
+  position: {
+    x: 30,
+    y: 45,
+  },
+  angle: 10,
+  data: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABmJLR0QA/wD/AP+gvaeTAAAAB3RJTUUH1ggDCwMADQ4NnwAAAFVJREFUGJWNkMEJADEIBEcbSDkXUnfSgnBVeZ8LSAjiwjyEQXSFEIcHGP9oAi+H0Bymgx9MhxbFdZE2a0s9kTZdw01ZhhYkABSwgmf1Z6r1SNyfFf4BZ+ZUExcNUQUAAAAASUVORK5CYII=',
 }
 
-const picture2 : PictureObject = {
-    id: "89bca59ceade4a5f92a3900fda5415e2",
-    type: SlideObjectType.PICTURE,
-    pictureType: PictureType.URL,
-    size: {
-        width: 250,
-        height: 250,
-    },
-    position: {
-        x: 0,
-        y: 0,
-    },
-    angle: 45,
-    data: "https://hips.hearstapps.com/hmg-prod/images/champagne-beach-espiritu-santo-island-vanuatu-royalty-free-image-1655672510.jpg",
+const picture2: PictureObject = {
+  id: '89bca59ceade4a5f92a3900fda5415e2',
+  type: SlideObjectType.PICTURE,
+  pictureType: PictureType.URL,
+  size: {
+    width: 250,
+    height: 250,
+  },
+  position: {
+    x: 0,
+    y: 0,
+  },
+  angle: 45,
+  data: 'https://hips.hearstapps.com/hmg-prod/images/champagne-beach-espiritu-santo-island-vanuatu-royalty-free-image-1655672510.jpg',
 }
 
-const picture3 : PictureObject = {
-    id: "131b368770a44fb3ab74b5f2b2280d09",
-    type: SlideObjectType.PICTURE,
-    pictureType: PictureType.BASE64,
-    size: {
-        width: 128,
-        height: 128,
-    },
-    position: {
-        x: 360,
-        y: 240,
-    },
-    angle: 90,
-    data: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABmJLR0QA/wD/AP+gvaeTAAAAB3RJTUUH1ggDCwMADQ4NnwAAAFVJREFUGJWNkMEJADEIBEcbSDkXUnfSgnBVeZ8LSAjiwjyEQXSFEIcHGP9oAi+H0Bymgx9MhxbFdZE2a0s9kTZdw01ZhhYkABSwgmf1Z6r1SNyfFf4BZ+ZUExcNUQUAAAAASUVORK5CYII=",
+const picture3: PictureObject = {
+  id: '131b368770a44fb3ab74b5f2b2280d09',
+  type: SlideObjectType.PICTURE,
+  pictureType: PictureType.BASE64,
+  size: {
+    width: 128,
+    height: 128,
+  },
+  position: {
+    x: 360,
+    y: 240,
+  },
+  angle: 90,
+  data: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABmJLR0QA/wD/AP+gvaeTAAAAB3RJTUUH1ggDCwMADQ4NnwAAAFVJREFUGJWNkMEJADEIBEcbSDkXUnfSgnBVeZ8LSAjiwjyEQXSFEIcHGP9oAi+H0Bymgx9MhxbFdZE2a0s9kTZdw01ZhhYkABSwgmf1Z6r1SNyfFf4BZ+ZUExcNUQUAAAAASUVORK5CYII=',
 }
 
-const text1 : TextObject = {
-    id: "188014653ffb49cd898a60c63acfaaf7",
-    type: SlideObjectType.TEXT,
-    size: {
-        width: 150,
-        height: 30,
+const text1: TextObject = {
+  id: '188014653ffb49cd898a60c63acfaaf7',
+  type: SlideObjectType.TEXT,
+  size: {
+    width: 150,
+    height: 30,
+  },
+  position: {
+    x: 300,
+    y: 100,
+  },
+  angle: 0,
+  text: 'Lorem ipsum dolor',
+  params: {
+    fontFamily: 'Roboto',
+    size: 12,
+    bold: true,
+    cursive: false,
+    color: {
+      r: 255,
+      g: 255,
+      b: 255,
+      a: 0.8,
     },
-    position: {
-        x: 300,
-        y: 100,
-    },
-    angle: 0,
-    text: "Lorem ipsum dolor",
-    params: {
-        fontFamily: "Roboto",
-        size: 12,
-        bold: true,
-        cursive: false, 
-        color: {
-            r: 255,
-            g: 255,
-            b: 255,
-            a: 0.8,
-        }
-    }
+  },
 }
 
-const text2 : TextObject = {
-    id: "83157e6a6fec4a5dbb27b95bcd68f3c2",
-    type: SlideObjectType.TEXT,
-    size: {
-        width: 25,
-        height: 100,
+const text2: TextObject = {
+  id: '83157e6a6fec4a5dbb27b95bcd68f3c2',
+  type: SlideObjectType.TEXT,
+  size: {
+    width: 25,
+    height: 100,
+  },
+  position: {
+    x: 500,
+    y: 500,
+  },
+  angle: 0,
+  text: 'Suspendisse et erat.',
+  params: {
+    fontFamily: 'Arial',
+    size: 16,
+    bold: false,
+    cursive: true,
+    color: {
+      r: 255,
+      g: 255,
+      b: 255,
+      a: 0.8,
     },
-    position: {
-        x: 500,
-        y: 500,
-    },
-    angle: 0,
-    text: "Suspendisse et erat.",
-    params: {
-        fontFamily: "Arial",
-        size: 16,
-        bold: false,
-        cursive: true,
-        color: {
-            r: 255,
-            g: 255,
-            b: 255,
-            a: 0.8,
-        }
-    }
+  },
 }
 
-const text3 : TextObject = {
-    id: "6bf260cf55cf4dfcb4e5fb667e759f18",
-    type: SlideObjectType.TEXT,
-    size: {
-        width: 125,
-        height: 125,
+const text3: TextObject = {
+  id: '6bf260cf55cf4dfcb4e5fb667e759f18',
+  type: SlideObjectType.TEXT,
+  size: {
+    width: 125,
+    height: 125,
+  },
+  position: {
+    x: 360,
+    y: 120,
+  },
+  text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, modi repellendus esse dicta labore iusto expedita ab officia debitis, ullam temporibus. Sequi, ducimus. Natus, quidem! Facere eos obcaecati ipsa, fugit quod nihil voluptates nemo animi ullam blanditiis veritatis mollitia excepturi. Voluptatibus velit est nisi nesciunt neque autem dolores vero, quo, sed aliquid in natus? Nostrum repellendus earum corporis perspiciatis exercitationem! Quisquam saepe ex voluptas. Qui, quasi eaque. Nesciunt asperiores culpa impedit odio et animi corrupti tempore iste odit ipsa ullam nostrum ducimus nobis provident harum deserunt quo at, nulla iure, praesentium omnis magni. Incidunt iure saepe, quisquam veniam pariatur vel!',
+  angle: 0,
+  params: {
+    fontFamily: 'Roboto',
+    size: 16,
+    bold: false,
+    cursive: true,
+    color: {
+      r: 220,
+      g: 220,
+      b: 220,
+      a: 1,
     },
-    position: {
-        x: 360,
-        y: 120,
-    },
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat, modi repellendus esse dicta labore iusto expedita ab officia debitis, ullam temporibus. Sequi, ducimus. Natus, quidem! Facere eos obcaecati ipsa, fugit quod nihil voluptates nemo animi ullam blanditiis veritatis mollitia excepturi. Voluptatibus velit est nisi nesciunt neque autem dolores vero, quo, sed aliquid in natus? Nostrum repellendus earum corporis perspiciatis exercitationem! Quisquam saepe ex voluptas. Qui, quasi eaque. Nesciunt asperiores culpa impedit odio et animi corrupti tempore iste odit ipsa ullam nostrum ducimus nobis provident harum deserunt quo at, nulla iure, praesentium omnis magni. Incidunt iure saepe, quisquam veniam pariatur vel!",
-    angle: 0,
-    params: {
-        fontFamily: "Roboto",
-        size: 16,
-        bold: false,
-        cursive: true,
-        color: {
-            r: 220,
-            g: 220,
-            b: 220,
-            a: 1,
-        }
-    }
+  },
 }
 
-const primitive1 : ShapeObject = {
-    id: "f6ba95e407ab48db939aeed916d4fc31",
-    type: SlideObjectType.PRIMITIVE,
-    shapeType: ShapeType.RECTANGLE,
-    size: {
-        width: 75,
-        height: 75,
-    },
-    position: {
-        x: 50,
-        y: 50,
-    },
-    angle: 0,
-    backgroundColor: {
-        r: 0,
-        g: 128,
-        b: 0,
-        a: 0.5,
-    },
-    borderColor: {
-        r: 255,
-        g: 255,
-        b: 255,
-        a: 0.8,
-    },
+const primitive1: ShapeObject = {
+  id: 'f6ba95e407ab48db939aeed916d4fc31',
+  type: SlideObjectType.PRIMITIVE,
+  shapeType: ShapeType.RECTANGLE,
+  size: {
+    width: 75,
+    height: 75,
+  },
+  position: {
+    x: 50,
+    y: 50,
+  },
+  angle: 0,
+  backgroundColor: {
+    r: 0,
+    g: 128,
+    b: 0,
+    a: 0.5,
+  },
+  borderColor: {
+    r: 255,
+    g: 255,
+    b: 255,
+    a: 0.8,
+  },
 }
 
-const primitive2 : ShapeObject = {
-    id: "73e2788497de4fd987696e79ccdd3977",
-    type: SlideObjectType.PRIMITIVE,
-    shapeType: ShapeType.TRIANGLE,
-    size: {
-        width: 120,
-        height: 30,
-    },
-    position: {
-        x: 30,
-        y: 200,
-    },
-    angle: 0,
-    backgroundColor: {
-        r: 255,
-        g: 255,
-        b: 255,
-        a: 0.2,
-    },
-    borderColor: {
-        r: 120,
-        g: 120,
-        b: 120,
-        a: 1,
-    },
+const primitive2: ShapeObject = {
+  id: '73e2788497de4fd987696e79ccdd3977',
+  type: SlideObjectType.PRIMITIVE,
+  shapeType: ShapeType.TRIANGLE,
+  size: {
+    width: 120,
+    height: 30,
+  },
+  position: {
+    x: 30,
+    y: 200,
+  },
+  angle: 0,
+  backgroundColor: {
+    r: 255,
+    g: 255,
+    b: 255,
+    a: 0.2,
+  },
+  borderColor: {
+    r: 120,
+    g: 120,
+    b: 120,
+    a: 1,
+  },
 }
 
-const primitive3 : ShapeObject = {
-    id: "c1770501005b4c5297da47724fc361e1",
-    type: SlideObjectType.PRIMITIVE,
-    shapeType: ShapeType.ELLIPSE,
-    size: {
-        width: 200,
-        height: 200,
-    },
-    position: {
-        x: 250,
-        y: 250,
-    },
-    angle: 120,
-    backgroundColor: {
-        r: 180,
-        g: 70,
-        b: 26,
-        a: 1,
-    },
-    borderColor: {
-        r: 200,
-        g: 200,
-        b: 200,
-        a: 0.5,
-    },
+const primitive3: ShapeObject = {
+  id: 'c1770501005b4c5297da47724fc361e1',
+  type: SlideObjectType.PRIMITIVE,
+  shapeType: ShapeType.ELLIPSE,
+  size: {
+    width: 200,
+    height: 200,
+  },
+  position: {
+    x: 250,
+    y: 250,
+  },
+  angle: 120,
+  backgroundColor: {
+    r: 180,
+    g: 70,
+    b: 26,
+    a: 1,
+  },
+  borderColor: {
+    r: 200,
+    g: 200,
+    b: 200,
+    a: 0.5,
+  },
 }
 
 const slide0: Slide = {
-    id: "90b877d2b36b454e820378127e8b9f7e",
-    background: {
-        type: SlideBackgroundType.PICTURE_BASE64,
-        data: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABmJLR0QA/wD/AP+gvaeTAAAAB3RJTUUH1ggDCwMADQ4NnwAAAFVJREFUGJWNkMEJADEIBEcbSDkXUnfSgnBVeZ8LSAjiwjyEQXSFEIcHGP9oAi+H0Bymgx9MhxbFdZE2a0s9kTZdw01ZhhYkABSwgmf1Z6r1SNyfFf4BZ+ZUExcNUQUAAAAASUVORK5CYII=",
-    },
-    objects: [],
+  id: '90b877d2b36b454e820378127e8b9f7e',
+  background: {
+    type: SlideBackgroundType.PICTURE_BASE64,
+    data: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABmJLR0QA/wD/AP+gvaeTAAAAB3RJTUUH1ggDCwMADQ4NnwAAAFVJREFUGJWNkMEJADEIBEcbSDkXUnfSgnBVeZ8LSAjiwjyEQXSFEIcHGP9oAi+H0Bymgx9MhxbFdZE2a0s9kTZdw01ZhhYkABSwgmf1Z6r1SNyfFf4BZ+ZUExcNUQUAAAAASUVORK5CYII=',
+  },
+  objects: [],
 }
 
 const slide1: Slide = {
-    id: "c932d26bcf6946618042e9852163c642",
-    background: {
-        type: SlideBackgroundType.PICTURE_BASE64,
-        data: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABmJLR0QA/wD/AP+gvaeTAAAAB3RJTUUH1ggDCwMADQ4NnwAAAFVJREFUGJWNkMEJADEIBEcbSDkXUnfSgnBVeZ8LSAjiwjyEQXSFEIcHGP9oAi+H0Bymgx9MhxbFdZE2a0s9kTZdw01ZhhYkABSwgmf1Z6r1SNyfFf4BZ+ZUExcNUQUAAAAASUVORK5CYII=",
-    },
-    objects: [ picture1, text1, primitive1 ],
+  id: 'c932d26bcf6946618042e9852163c642',
+  background: {
+    type: SlideBackgroundType.PICTURE_BASE64,
+    data: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAABmJLR0QA/wD/AP+gvaeTAAAAB3RJTUUH1ggDCwMADQ4NnwAAAFVJREFUGJWNkMEJADEIBEcbSDkXUnfSgnBVeZ8LSAjiwjyEQXSFEIcHGP9oAi+H0Bymgx9MhxbFdZE2a0s9kTZdw01ZhhYkABSwgmf1Z6r1SNyfFf4BZ+ZUExcNUQUAAAAASUVORK5CYII=',
+  },
+  objects: [picture1, text1, primitive1],
 }
 
 const slide2: Slide = {
-    id: "4713d4539004469d8ac1bb0afd408f25",
-    background: {
-        type: SlideBackgroundType.SOLID_COLOR,
-        color: {
-            r: 255,
-            g: 255,
-            b: 255,
-            a: 0.1,
-        }
+  id: '4713d4539004469d8ac1bb0afd408f25',
+  background: {
+    type: SlideBackgroundType.SOLID_COLOR,
+    color: {
+      r: 255,
+      g: 255,
+      b: 255,
+      a: 0.1,
     },
-    objects: [ picture2, text2, primitive2 ],
+  },
+  objects: [picture2, text2, primitive2],
 }
 
 const slide3: Slide = {
-    id: "c1d4b504d20a44b9a104709adfd29839",
-    background: {
-        type: SlideBackgroundType.PICTURE_URL,
-        data: "https://hips.hearstapps.com/hmg-prod/images/champagne-beach-espiritu-santo-island-vanuatu-royalty-free-image-1655672510.jpg",
-    },
-    objects: [ picture3, text3, primitive3 ],
+  id: 'c1d4b504d20a44b9a104709adfd29839',
+  background: {
+    type: SlideBackgroundType.PICTURE_URL,
+    data: 'https://hips.hearstapps.com/hmg-prod/images/champagne-beach-espiritu-santo-island-vanuatu-royalty-free-image-1655672510.jpg',
+  },
+  objects: [picture3, text3, primitive3],
 }
 
 const slide4: Slide = {
-    id: "9741ab9d1b7e40ae8d1004eaf73edd21",
-    background: {
-        type: SlideBackgroundType.SOLID_COLOR,
-        color: {
-            r: 60,
-            g: 60,
-            b: 60,
-            a: 1,
-        }
+  id: '9741ab9d1b7e40ae8d1004eaf73edd21',
+  background: {
+    type: SlideBackgroundType.SOLID_COLOR,
+    color: {
+      r: 60,
+      g: 60,
+      b: 60,
+      a: 1,
     },
-    objects: [ text1, text2, text3 ],
+  },
+  objects: [text1, text2, text3],
 }
 
 const slide5: Slide = {
-    id: "adbf79efcf1f49b980fdaf2e8775601a",
-    background: {
-        type: SlideBackgroundType.SOLID_COLOR,
-        color: {
-            r: 235,
-            g: 235,
-            b: 235,
-            a: 1,
-        }
+  id: 'adbf79efcf1f49b980fdaf2e8775601a',
+  background: {
+    type: SlideBackgroundType.SOLID_COLOR,
+    color: {
+      r: 235,
+      g: 235,
+      b: 235,
+      a: 1,
     },
-    objects: [ picture3 ],
+  },
+  objects: [picture3],
 }
 
 const slide6: Slide = {
-    id: "18e8ae4602194c5ea369749e464d2f28",
-    background: {
-        type: SlideBackgroundType.PICTURE_BASE64,
-        data: "data:image/jpg;base64,aHR0cHM6Ly90bi5maXNoa2kubmV0LzIwL3ByZXZpZXcvMjg5NzMzMy5qcGc=",
-    },
-    objects: [ primitive1, primitive2, primitive3 ],
+  id: '18e8ae4602194c5ea369749e464d2f28',
+  background: {
+    type: SlideBackgroundType.PICTURE_BASE64,
+    data: 'data:image/jpg;base64,aHR0cHM6Ly90bi5maXNoa2kubmV0LzIwL3ByZXZpZXcvMjg5NzMzMy5qcGc=',
+  },
+  objects: [primitive1, primitive2, primitive3],
 }
 
 const slide7: Slide = {
-    id: "5fd3ef9443244fc2a19c33c0499ecf69",
-    background: {
-        type: SlideBackgroundType.PICTURE_URL,
-        data: "https://sportishka.com/uploads/posts/2022-04/1650706226_1-sportishka-com-p-islandiya-vikipediya-krasivo-foto-1.jpg",
-    },
-    objects: [ picture1, text3, picture3 ],
+  id: '5fd3ef9443244fc2a19c33c0499ecf69',
+  background: {
+    type: SlideBackgroundType.PICTURE_URL,
+    data: 'https://sportishka.com/uploads/posts/2022-04/1650706226_1-sportishka-com-p-islandiya-vikipediya-krasivo-foto-1.jpg',
+  },
+  objects: [picture1, text3, picture3],
 }
 
 const document1: Doc = {
-    title: "presentation1",
-    slideList: [slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9, slide10],
+  title: 'presentation1',
+  slideList: [
+    slide1,
+    slide2,
+    slide3,
+    slide4,
+    slide5,
+    slide6,
+    slide7,
+    slide8,
+    slide9,
+    slide10,
+  ],
 }
 
 const document2: Doc = {
-    title: "presentation2",
-    slideList: [slide1, slide0],
+  title: 'presentation2',
+  slideList: [slide1, slide0],
 }
 
 const document3: Doc = {
-    title: "presentation3",
-    slideList: [slide0],
+  title: 'presentation3',
+  slideList: [slide0],
 }
 
 const editor1: Editor = {
-    document: document1,
-    history: [],
-    currentSlide: "c1d4b504d20a44b9a104709adfd29839",
-    selected: {
-        slideIds: ["c932d26bcf6946618042e9852163c642", "c1d4b504d20a44b9a104709adfd29839"]
-    }
+  document: document1,
+  history: [],
+  currentSlide: 'c1d4b504d20a44b9a104709adfd29839',
+  selected: {
+    slideIds: [
+      'c932d26bcf6946618042e9852163c642',
+      'c1d4b504d20a44b9a104709adfd29839',
+    ],
+  },
 }
 
 const editor2: Editor = {
-    document: document2,
-    history: [],
-    currentSlide: "c932d26bcf6946618042e9852163c642",
-    selected: {
-        slideObjectIds: ["188014653ffb49cd898a60c63acfaaf7"]
-    }
+  document: document2,
+  history: [],
+  currentSlide: 'c932d26bcf6946618042e9852163c642',
+  selected: {
+    slideObjectIds: ['188014653ffb49cd898a60c63acfaaf7'],
+  },
 }
 
 const editor3: Editor = {
-    document: document3,
-    history: [],
-    currentSlide: "c1d4b504d20a44b9a104709adfd29839",
-    selected: {
-        slideObjectIds: []
-    }
+  document: document3,
+  history: [],
+  currentSlide: 'c1d4b504d20a44b9a104709adfd29839',
+  selected: {
+    slideObjectIds: [],
+  },
 }
--- types.ts
+++ types.ts
@@ -1,127 +1,126 @@
 enum SlideBackgroundType {
-    SOLID_COLOR,
-    PICTURE_BASE64,
-    PICTURE_URL
+  SOLID_COLOR,
+  PICTURE_BASE64,
+  PICTURE_URL,
 }
 
 enum SlideObjectType {
-    TEXT,
-    PICTURE,
-    PRIMITIVE,
+  TEXT,
+  PICTURE,
+  PRIMITIVE,
 }
 
 enum ShapeType {
-    RECTANGLE,
-    TRIANGLE,
-    ELLIPSE,
+  RECTANGLE,
+  TRIANGLE,
+  ELLIPSE,
 }
 
 enum PictureType {
-    BASE64,
-    URL,
+  BASE64,
+  URL,
 }
 
 type Editor = {
-    document: Doc,
-    history: HistoryItem[],
-    currentSlide: string,
-    selected: SelectedObjects | SelectedSlides,
+  document: Doc
+  history: HistoryItem[]
+  currentSlide: string
+  selected: SelectedObjects | SelectedSlides
 }
 
 type SelectedObjects = {
-    slideObjectIds: string[],
+  slideObjectIds: string[]
 }
 
 type SelectedSlides = {
-    slideIds: string[],
+  slideIds: string[]
 }
 
 type Doc = {
-    title: string,
-    slideList: Slide[],
+  title: string
+  slideList: Slide[]
 }
 
 type HistoryItem = {
-    // hist item props
+  // hist item props
 }
 
 type Position = {
-    x: number,
-    y: number,
+  x: number
+  y: number
 }
 
 type Size = {
-    width: number,
-    height: number,
+  width: number
+  height: number
 }
 
 type Color = {
-    r: number,
-    g: number,
-    b: number,
-    a: number,
+  r: number
+  g: number
+  b: number
+  a: number
 }
 
 type Slide = {
-    id: string,
-    background: BackgroundSolid | BackgroundPicture,
-    objects: (TextObject | PictureObject | ShapeObject)[],
+  id: string
+  background: BackgroundSolid | BackgroundPicture
+  objects: (TextObject | PictureObject | ShapeObject)[]
 }
 
 type BackgroundSolid = {
-    type: SlideBackgroundType.SOLID_COLOR,
-    color: Color,
+  type: SlideBackgroundType.SOLID_COLOR
+  color: Color
 }
 
 type BackgroundPicture = {
-    type: SlideBackgroundType.PICTURE_BASE64 | SlideBackgroundType.PICTURE_URL,
-    data: string,
+  type: SlideBackgroundType.PICTURE_BASE64 | SlideBackgroundType.PICTURE_URL
+  data: string
 }
 
 type SlideObjectBase = {
-    id: string,
-    size: Size,
-    position: Position,
-    angle: number,
+  id: string
+  size: Size
+  position: Position
+  angle: number
 }
 
 type TextParam = {
-    fontFamily: string,
-    size: number,
-    bold: boolean,
-    cursive: boolean, 
-    color: Color,
+  fontFamily: string
+  size: number
+  bold: boolean
+  cursive: boolean
+  color: Color
 }
 
 type TextObject = SlideObjectBase & {
-    type: SlideObjectType.TEXT,
-    params: TextParam,
-    text: string,
+  type: SlideObjectType.TEXT
+  params: TextParam
+  text: string
 }
 
 type PictureObject = SlideObjectBase & {
-    type: SlideObjectType.PICTURE,
-    pictureType: PictureType,
-    data: string,
+  type: SlideObjectType.PICTURE
+  pictureType: PictureType
+  data: string
 }
 
 type ShapeObject = SlideObjectBase & {
-    type: SlideObjectType.PRIMITIVE,
-    shapeType: ShapeType,
-    backgroundColor: Color,
-    borderColor: Color,
+  type: SlideObjectType.PRIMITIVE
+  shapeType: ShapeType
+  backgroundColor: Color
+  borderColor: Color
 }
 
 export {
-    SlideObjectType,
-    SlideBackgroundType,
-    ShapeType,
-    PictureType,
-    ShapeObject,
-    PictureObject,
-    TextObject,
-    Slide,
-    Doc,
-    Editor
+  SlideObjectType,
+  SlideBackgroundType,
+  ShapeType,
+  PictureType,
+  ShapeObject,
+  PictureObject,
+  TextObject,
+  Slide,
+  Doc,
+  Editor,
 }
-
