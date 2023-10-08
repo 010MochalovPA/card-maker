0b0705907f33a29bba5808f933ce039728ef2eba
Pavel Mochalov <010mochalovpa@gmail.com>
2023-10-08 12:12:48 +0300

    подключил react webpack babel и тд. Без create-react-app

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import styles from './App.css'
+
+const App = () => {
+  return <div className={styles.test}>Hello</div>
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+const webpack = require('webpack')
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: { script: path.resolve(__dirname, './src/') },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.m?ts$|\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            onlyCompileBundledFiles: true,
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.*', '.js', '.jsx', '.tsx', '.ts', '.css'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'PresentationMaker',
+      template: './src/index.html',
+    }),
+  ]
+}
