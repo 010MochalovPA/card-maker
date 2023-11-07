dafed70d46e69d2392e765ca68b0f92c0e0390d2
vladislav@book <vladovinkin@gmail.com>
2023-11-07 23:26:00 +0300

    - попытка вывести примитивы (неудачная - не работат пути до изображений)

--- src/components/ObjectShape/ObjectShape.tsx
+++ src/components/ObjectShape/ObjectShape.tsx
@@ -10,8 +10,35 @@ type objectShapeProps = {
   }
 }
 
+const getShapeUrl = (shapeType: ShapeType) => {
+  switch (shapeType) {
+    case ShapeType.ELLIPSE:
+      return '../../assets/svg/circle.svg'
+    case ShapeType.RECTANGLE:
+      return '../../assets/svg/rectangle.svg'
+    case ShapeType.TRIANGLE:
+      return '../../assets/svg/triangle.svg'
+  }
+}
+
 const ObjectShape = (props: objectShapeProps) => {
-  return <div className={styles.shape}></div>
+  const {size, position, angle, borderColor, backgroundColor, shapeType} = props.props
+  const {width, height} = size
+  const {left, top} = position
+  const backgroundSize = `${width}px ${height}px`
+  const background = `transparent url(${getShapeUrl(shapeType)}) no-repeat top left`
+
+  return <div
+    className={styles.shape}
+    style={{
+      width,
+      height,
+      left,
+      top,
+      background,
+      backgroundSize,
+    }}
+  ></div>
 }
 
 export default ObjectShape
