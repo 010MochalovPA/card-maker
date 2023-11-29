5521e20b0623c054e2b3d012c62572e233c28e2e
vladislav@book <vladovinkin@gmail.com>
2023-11-29 16:59:11 +0300

    - переключил editor - попробовал сделать отображение при переносе текстового объекта

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import Layout from '../Layout/Layout'
-import { editor2 } from '../../mock'
+import { editor1 } from '../../mock'
 import { EditorContext } from '../../context/editorContext'
 import { useMemo, useState } from 'react';
 
 const App = () => {
-  const [editor, setEditor] = useState(editor2);
+  const [editor, setEditor] = useState(editor1);
   const update = useMemo(() => {return {editor, setEditor}}, [editor])
   return (
       <EditorContext.Provider value={update}>
