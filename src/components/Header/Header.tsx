c1d7f65838b3470730db4cfd1be19ec05339e508
Pavel Mochalov <010mochalovpa@gmail.com>
2024-01-05 17:26:09 +0300

    начал export

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,13 @@ import PresentationTitle from '../PresentationTitle/PresentationTitle'
 import styles from './Header.css'
 import { ToolbarFile } from '../ToolbarFile/ToolbarFile'
 import { ToolbarHistory } from '../ToolbarHistory/ToolbarHistory'
+import { useState } from 'react'
+import { Modal } from '../Modal/Modal'
+import PreviewPDF from '../PreviewPDF/PreviewPDF'
 
 const Header = () => {
-
+  const [isPreviewOpen, setIsPreviewOpen] = useState(false)
+  
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
@@ -17,7 +21,12 @@ const Header = () => {
       <ToolbarHistory />
       <PresentationTitle />
       <div className={styles.spacer}></div>
-      <ButtonHeader icon={FileExport} text={'Экспорт в PDF'} onClick={() => console.log('file export')} />
+      <ButtonHeader icon={FileExport} text={'Экспорт в PDF'} onClick={() => setIsPreviewOpen(true)} />
+      {isPreviewOpen && (
+        <Modal onClose={() => setIsPreviewOpen(false)}>
+          <PreviewPDF/>
+        </Modal>
+      )}
     </div>
   )
 }
