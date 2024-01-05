9708ffc3f160480eba7145ed6457fcf0e419b88d
vladislav@book <vladovinkin@gmail.com>
2024-01-06 00:14:19 +0300

    кнопка добавления слайда после превью слайдов

--- src/components/SlidesPanel/SlidesPanel.tsx
+++ src/components/SlidesPanel/SlidesPanel.tsx
@@ -1,5 +1,7 @@
 import styles from './SlidesPanel.css'
 import SlidePreview from '../SlidePreview/SlidePreview'
+import SlideNewPlus from '../../icons/SlideNewPlus'
+import { useAppActions } from '../../redux/hooks'
 
 type SlidesListType = {
   id: string
@@ -11,20 +13,28 @@ type SlidesPanelPropsType = {
   slideList: SlidesListType[]
 }
 
-const SlidesPanel = ({ slideList }: SlidesPanelPropsType) => (
-  <div className={styles.slidesPanel}>
-    <div className={styles.slidesWrapper}>
-      {slideList.map((slideItem, index) => (
-        <SlidePreview
-          key={slideItem.id}
-          index={index}
-          slideId={slideItem.id}
-          onClick={slideItem.onClick}
-          isActive={slideItem.isActive}
-        />
-      ))}
+const SlidesPanel = ({ slideList }: SlidesPanelPropsType) => {
+  const { createAddSlideAction } =
+    useAppActions()
+
+  return (
+    <div className={styles.slidesPanel}>
+      <div className={styles.slidesWrapper}>
+        {slideList.map((slideItem, index) => (
+          <SlidePreview
+            key={slideItem.id}
+            index={index}
+            slideId={slideItem.id}
+            onClick={slideItem.onClick}
+            isActive={slideItem.isActive}
+          />
+        ))}
+        <div className={styles.newSlideButton} title={'Add slide'} onClick={createAddSlideAction}>
+          <SlideNewPlus />
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default SlidesPanel
